afe8c898719b418145ac7a7129d621ae0ad66244
Antoine <An.soorbeek@laposte.net>
2016-02-11 15:09:52 +0100

    add avatar to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211134351) do
+ActiveRecord::Schema.define(version: 20160211140859) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160211134351) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
